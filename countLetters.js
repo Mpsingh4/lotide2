317c0bfd73184c92d80d7952c22830a48fc9bd8d
Manpreet <mp_manpreet@hotmail.com>
2023-03-10 03:26:08 +0000

    implement count letters

---
+++ countLetters.js
@@ -0,0 +1,21 @@
+const assertEquals = require("./assertEqual");
+
+const countLetters = function(str) {
+  const upperCaseStr = str.toUpperCase();
+  let result = {};
+  for (let letter of upperCaseStr) {
+    if (result[letter]) {
+      result[letter]++
+      } else {
+        result[letter] = 1;
+      }
+    }
+    let output = '';
+    for (let key in result) {
+      output += `${key}: ${result[key]}, `;
+    }
+    return output;
+}
+
+console.log(countLetters('LHLWP5pp'));
+console.log(countLetters("lighthouse in the house"));
