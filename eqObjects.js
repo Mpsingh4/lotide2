fab23da02250de95c54fce38dac2a1df72db7222
Manpreet <mp_manpreet@hotmail.com>
2023-03-10 04:54:24 +0000

    implement equal objects

---
+++ eqObjects.js
@@ -0,0 +1,39 @@
+const assertEqual = require('./assertEqual');
+const eqArrays = require('./eqArrays')
+
+const eqObjects = function(object1, object2) {
+  let key1 = Object.keys(object1);
+  let key2 = Object.keys(object2);
+  
+  if (key1.length !== key2.length) {
+    return false;
+  }
+
+  for (const key of key1) {
+    if (typeof object1[key] === 'object' && typeof object2[key] === 'object') {
+      if (!eqObjects(object1[key], object2[key])) {
+        return false;
+      }
+    } else if (object1[key] !== object2[key]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const shirtObject = { color: "red", size: "medium" };
+const anotherShirtObject= { size: "medium", color: "red" };
+console.log(eqObjects(shirtObject , anotherShirtObject)); // => true
+assertEqual(eqObjects(shirtObject , anotherShirtObject), true)
+
+const longSleeveShirtObject= { size: "medium", color: "red", sleeveLength: "long" };
+console.log(eqObjects(shirtObject , longSleeveShirtObject)); // => false
+assertEqual(eqObjects(shirtObject , longSleeveShirtObject), false);
+
+const multiColorShirtObject = { colors: ["red", "blue"], size: "medium" };
+const anotherMultiColorShirtObject = { size: "medium", colors: ["red", "blue"] };
+console.log(eqObjects(multiColorShirtObject  , anotherMultiColorShirtObject)); // => true
+
+const longSleeveMultiColorShirtObject= { size: "medium", colors: ["red", "blue"], sleeveLength: "long" };
+console.log(eqObjects(multiColorShirtObject  , longSleeveMultiColorShirtObject)); // => false
