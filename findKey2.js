c92f32dd68fea9a3dc71a5df91fc8ce5b82d21d0
Manpreet <mp_manpreet@hotmail.com>
2023-03-12 21:09:39 +0000

    not to be confused with find key by value under findkey.js

---
+++ findKey2.js
@@ -0,0 +1,22 @@
+const assertEqual = require('./assertEqual');
+
+const findKey = function(obj, value) {
+  let result = []
+  for (let key in obj) {
+    if (value(obj[key])) {
+      result.push(key);
+    }
+  }
+  console.log(result);
+  return result;
+}
+
+
+findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2) // => "noma"
