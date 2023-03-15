cd7cfce7cb19d02a6ace0987b6d201533379415e
Manpreet <mp_manpreet@hotmail.com>
2023-03-15 01:53:31 +0000

    added index

--- test/middleTest.js
+++ test/middleTest.js
@@ -3,7 +3,7 @@ const middle = require('../middle');
 
 describe("#middle", () => {
   it('returns 3 for [1, 2, 3, 4, 5]', () => {
-    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), );
   });
 });
 
